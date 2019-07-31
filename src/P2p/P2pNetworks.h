627ef6cfae03ed78c0fff6c3cd11bd8c39387482
user <ivighenie@yahoo.com>
2019-07-31 11:28:48 -0400

    upload files

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x47, 0x43, 0x41, 0x53, 0x48, 0x62, 0x65, 0x73, 0x74, 0x63, 0x72, 0x79, 0x70, 0x74, 0x6F, 0x21 } };
+}
