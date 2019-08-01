55aa5603ddbd9c6f653a26bfb1f0c656a425df27
Cristian <43420841+0xCristianOff@users.noreply.github.com>
2019-08-01 11:09:36 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -22,7 +22,7 @@ const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2;
 
 const size_t   BLOCKCHAIN_TIMESTAMP_CHECK_WINDOW             = 60;
 
-const uint64_t MONEY_SUPPLY 				     = ((uint64_t)(100000000)) ;
+const uint64_t MONEY_SUPPLY 				     = ((uint64_t)(5800000000000000)) ;
 const unsigned EMISSION_SPEED_FACTOR                         = 20;
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
